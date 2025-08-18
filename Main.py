9c9d1f34686c769ffce2314c2ba66d24716e1036
Jason <jsong2506@gmail.com>
2025-08-18 16:57:40 +0800

    Added a situation where user has active MobSF instance

--- Main.py
+++ Main.py
@@ -104,13 +104,17 @@ def process_json_file():
                 apk_checks.process_apk_result(data)
             print(f"{GREEN}[+] Done reviewing result for {data['file_name']}{RESET}")
 
-start_mobsf()
-
-API_KEY = get_mobsf_api_key()
-
-if API_KEY == "":
-    print("{RED}[-] Failed to extract MobSF API Key from logs.{RESET}")
-    exit(1)
+mobsf_already_on = input("[*] Do you have a current MobSF instance running? (Y/N): ")
+if (mobsf_already_on == "Y"):
+    print("ON")
+    MOBSF_URL = input("[*] Please enter the full URL (E.g. http://127.0.0.1:8000): ")
+    API_KEY = input("[*] Please input the API key: ")
+else:
+    start_mobsf()
+    API_KEY = get_mobsf_api_key()
+    if API_KEY == "":
+        print("{RED}[-] Failed to extract MobSF API Key from logs.{RESET}")
+        exit(1)
 
 headers={'Authorization': API_KEY}
 print("[*] Starting MobSF. Please wait for a minute")
